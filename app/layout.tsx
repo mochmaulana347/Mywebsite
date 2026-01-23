f8e747358b17adbe4b5fa52e58479a3ae9d85d3c
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-24 00:13:16 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import './globals.css'
+
 export const metadata = {
   title: 'Virtue ID',
   description: 'Digital Solutions',
@@ -10,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-slate-950">{children}</body>
     </html>
   )
 }
