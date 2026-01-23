9923b6a0a8b80061785f92d308b2c31ea4748d1b
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-24 00:29:42 +0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <main className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">VIRTUE ID IS COMING SOON</h1>
-    </main>
-  )
-}
